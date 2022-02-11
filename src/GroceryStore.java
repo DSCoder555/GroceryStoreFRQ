e9d2d3599f3e2f31aed7fa2b94a55987bc0110cd
BT_1E10_09 <dariusj0714@gmail.com>
2022-02-11 13:19:07 -0500

    Finished FRQ

--- src/GroceryStore.java
+++ src/GroceryStore.java
@@ -18,6 +18,13 @@ public class GroceryStore
     public ArrayList<Integer> getReorderList(int min)
     {
         /* to be implemented in part (a) */
+        ArrayList<Integer> reorderList = new ArrayList<Integer>();
+        for (int i = 0; i < productsStocked.length; i++) {
+            if(productsStocked[i].getQuantity() <= min){
+                reorderList.add(i);
+            }
+        }
+        return reorderList;
     }
 
     /** Returns true if all products named in shoppingList are available for purchase
@@ -28,5 +35,29 @@ public class GroceryStore
     public boolean checkAvailability(ArrayList<String> shoppingList)
     {
         /* to be implemented in part (b) */
+        for (String s : shoppingList) {
+            for (Product product : productsStocked) {
+                if (product.getName().equals(s) && product.getQuantity() == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
+
+    /** Returns an ArrayList containing all Products from productStocked that
+     *  have a weight that exceeds the “weight” value provided in the parameter
+     */
+    public ArrayList<Product> oversized(double weight)
+    {
+        ArrayList<Product> overWeight = new ArrayList<Product>();
+        for (Product product : productsStocked) {
+            if (product.getWeight() > weight) {
+                overWeight.add(product);
+            }
+
+        }
+        return overWeight;
+    }
+
 }
